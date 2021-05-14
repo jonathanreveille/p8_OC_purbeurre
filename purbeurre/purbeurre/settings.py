daa9417f1b0cfba0ff36d8edfc85fd8499d7dca1
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2021-05-14 14:22:15 +0200

    corrected  staticfildirs

--- purbeurre/purbeurre/settings.py
+++ purbeurre/purbeurre/settings.py
@@ -149,7 +149,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
-        os.path.join(BASE_DIR, 'static/dist'),
+        # os.path.join(BASE_DIR, 'static/dist'),
         ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
