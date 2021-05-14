81c7b9a44d184bac93b75d162f45311105d18cbd
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2021-05-14 12:38:55 +0200

    modification

--- purbeurre/purbeurre/settings.py
+++ purbeurre/purbeurre/settings.py
@@ -152,7 +152,10 @@ STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static')
         ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+#Maybe add this?
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL  = 'login'
