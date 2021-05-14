4828277cfd173a9e4a48d9a192ed027c763ab826
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2021-05-14 12:29:05 +0200

    prepare for deployment

--- purbeurre/purbeurre/settings.py
+++ purbeurre/purbeurre/settings.py
@@ -152,6 +152,7 @@ STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static')
         ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL  = 'login'
